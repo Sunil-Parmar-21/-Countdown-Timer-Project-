262fc74af61dc6b99909072f22ef6466b6dc4638
Sunil Parmar <161698152+Sunil-Parmar-21@users.noreply.github.com>
2024-12-20 15:59:45 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+const endDate =  "28 Dec 2024 02:30 PM" 
+
+document.getElementById("end-date").innerText = endDate;
+
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600 ) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+};
+
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+);
